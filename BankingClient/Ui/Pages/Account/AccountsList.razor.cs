0614e74882bf911d081eb5e9fed45c419c007b6a
Bernd-Uwe Rogalla <b-u.rogalla@ostfalia.de>
2025-02-05 11:46:02 +0100

    Missing files

---
+++ BankingClient/Ui/Pages/Account/AccountsList.razor.cs
@@ -0,0 +1,38 @@
+using BankingClient.Core;
+using BankingClient.Core.Dto;
+using BankingClient.Services;
+using Microsoft.AspNetCore.Components;
+namespace BankingClient.Ui.Pages.Account;
+
+public partial class AccountsList(
+    IAccountService accountService,
+    UserStateHolder userStateHolder,
+    NavigationManager navigationManager,
+    ILogger<AccountsList> logger
+): ComponentBase {
+    private List<AccountDto> _accountDtos = [];
+    private string? _errorMessage = null;
+
+    protected override async Task OnInitializedAsync() {
+        if (!userStateHolder.IsAuthenticated) {
+            _errorMessage = "Admin ist nicht angemeldet!";
+            return;
+        }
+
+        var resultData = await accountService.GetAll();
+        switch (resultData) {
+            case ResultData<IEnumerable<AccountDto>?>.Success sucess:
+                logger.LogInformation("OwnerList: GetAll");
+                _accountDtos = sucess.Data!.OrderBy(a => a.Iban).ToList();
+                break;
+            case ResultData<IEnumerable<AccountDto>?>.Error error:
+                _errorMessage = error.Exception.Message;
+                return;
+        }
+    }
+    
+    private void OpenAccount(Guid accountId) {
+        logger.LogInformation("OwnerList: nav: /accounts/{1}", accountId);
+        navigationManager.NavigateTo($"/accounts/{accountId}");
+    }
+}

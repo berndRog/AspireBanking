6b2b3618e700d150dcf73441e007a50276a892ff
Bernd-Uwe Rogalla <b-u.rogalla@ostfalia.de>
2025-02-12 17:06:43 +0100

    further clean up, deleting People, User ...

--- BankingClient/Core/ImagesService.cs
+++
@@ -1,8 +0,0 @@
-namespace BankingClient.Core;
-
-public interface ImagesService {
-   Task<bool> FindImage(string imageUrl);
-   Task<(byte[]?, string, string)> GetImage(string imageUrl);
-   Task<string?> PostImage(MultipartFormDataContent content);
-   Task<bool> DeleteImage(string imageUrl);
-}
